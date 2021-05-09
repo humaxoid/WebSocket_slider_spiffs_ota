28df63560270199dea5edbaecb151320282d10b9
humaxoid <83541067+humaxoid@users.noreply.github.com>
2021-05-10 00:11:58 +0300

    Update script.js
    In Javascript, you need to recognize which LED the notification is intended for. Made using event.data
    (event.data == "1")
    (event.data == "3")

--- data/script.js
+++ data/script.js
@@ -9,53 +9,26 @@ var gateway = `ws://${window.location.hostname}/ws`;
     websocket.onmessage = onMessage;
   }
   
-  function onOpen(event) {
-    console.log('Connection opened');
-  }
-  
-  function onClose(event) {
-    console.log('Connection closed');
-    setTimeout(initWebSocket, 2000);
-  }
+  function onOpen(event) { console.log('Connection opened'); }
+  function onClose(event) { console.log('Connection closed'); setTimeout(initWebSocket, 2000); }
  
   function onMessage(event) {
-	 var state1;
-    if (event.data == "1"){
-    state1 = "ON";
-//	  document.getElementById('button1').style.backgroundColor = "#04b50a";
+    var state1; if (event.data == "1"){ state1 = "ON";
+    // document.getElementById('button1').style.backgroundColor = "#04b50a";
+    } else { state1 = "OFF"; // document.getElementById('button1').style.backgroundColor = "#c90411";
     }
-   else{
-     state1 = "OFF";
-//	  document.getElementById('button1').style.backgroundColor = "#c90411";
-    }
-    document.getElementById('state1').innerHTML = state1;
+      document.getElementById('state1').innerHTML = state1;
 //====
-	 var state2;
-    if (event.data == "1"){
-    state2 = "ON";
-//	  document.getElementById('button2').style.backgroundColor = "#04b50a";
-    }
-   else{
-     state2 = "OFF";
-//	  document.getElementById('button2').style.backgroundColor = "#c90411";
-    }
-    document.getElementById('state2').innerHTML = state2;
+   var state2; if (event.data == "3"){ state2 = "ON";} else {state2 = "OFF";}
+     document.getElementById('state2').innerHTML = state2;
   }
  
-  function onLoad(event) {
-    initWebSocket();
-	initButton();
-  }
+  function onLoad(event) { initWebSocket(); initButton(); }
 
   function initButton() {
     document.getElementById('button1').addEventListener('click', toggle1);
-	document.getElementById('button2').addEventListener('click', toggle2);
-  }
+    document.getElementById('button2').addEventListener('click', toggle2);
+   }
    
-  function toggle1(){
-    websocket.send('toggle1');
-  }
-  
-  function toggle2(){
-    websocket.send('toggle2');
-  }
+  function toggle1(){ websocket.send('toggle1'); }
+  function toggle2(){ websocket.send('toggle2'); }
