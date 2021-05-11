5a3420df6da961e0bdf6ddd151a0dc7f5445066e
humaxoid <satman69@mail.ru>
2021-05-11 21:23:43 +0300

    3 buttons added
    The problem with displaying the status of all enabled buttons is resolved.

--- data/script.js
+++ data/script.js
@@ -1,4 +1,4 @@
-var gateway = `ws://${window.location.hostname}/ws`;
+  var gateway = `ws://${window.location.hostname}/ws`;
   var websocket;
   window.addEventListener('load', onLoad);
   function initWebSocket() {
@@ -9,26 +9,33 @@ var gateway = `ws://${window.location.hostname}/ws`;
     websocket.onmessage = onMessage;
   }
   
-  function onOpen(event) { console.log('Connection opened'); }
-  function onClose(event) { console.log('Connection closed'); setTimeout(initWebSocket, 2000); }
- 
-  function onMessage(event) {
-    var state1; if (event.data == "1"){ state1 = "ON";
-    // document.getElementById('button1').style.backgroundColor = "#04b50a";
-    } else { state1 = "OFF"; // document.getElementById('button1').style.backgroundColor = "#c90411";
-    }
-      document.getElementById('state1').innerHTML = state1;
-//====
-   var state2; if (event.data == "3"){ state2 = "ON";} else {state2 = "OFF";}
-     document.getElementById('state2').innerHTML = state2;
+  function onOpen(event) {
+    console.log('Connection opened');
+  }
+  function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
   }
  
-  function onLoad(event) { initWebSocket(); initButton(); }
+  function onMessage(event) { 
+    switch(event.data) {
+      case '0': document.getElementById("state1").innerHTML = "OFF"; break
+      case '1': document.getElementById("state1").innerHTML = "ON"; break
+      case '2': document.getElementById("state2").innerHTML = "OFF"; break
+      case '3': document.getElementById("state2").innerHTML = "ON"; break
+      case '4': document.getElementById("state3").innerHTML = "OFF"; break
+      case '5': document.getElementById("state3").innerHTML = "ON"; break
+    }
+    } 
+  
+  function onLoad(event) {initWebSocket();initButton();}
 
   function initButton() {
     document.getElementById('button1').addEventListener('click', toggle1);
-    document.getElementById('button2').addEventListener('click', toggle2);
-   }
-   
-  function toggle1(){ websocket.send('toggle1'); }
-  function toggle2(){ websocket.send('toggle2'); }
+	document.getElementById('button2').addEventListener('click', toggle2);
+	document.getElementById('button3').addEventListener('click', toggle3);
+  }
+  
+  function toggle1(){websocket.send('toggle1');}
+  function toggle2(){websocket.send('toggle2');}
+  function toggle3(){websocket.send('toggle3');}
--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,54 @@
-// Import required libraries
+// Import required libraries for ESP32
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
-// Задаем сетевые настройки
-const char* ssid = "Uname";
+// Network settings
+const char* ssid = "uname";
 const char* password = "pass";
-IPAddress local_IP(192, 168, 11, 68);
-IPAddress gateway(192, 168, 11, 102);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(8, 8, 8, 8);
-IPAddress secondaryDNS(8, 8, 4, 4); 
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
+IPAddress local_IP(192, 168, 1, 6);    // Задаем статический IP-адрес:
+IPAddress gateway(192, 168, 1, 1);     // Задаем IP-адрес сетевого шлюза:
+IPAddress subnet(255, 255, 255, 0);    // Задаем маску сети:
+IPAddress primaryDNS(8, 8, 8, 8);      // Основной ДНС (опционально)
+IPAddress secondaryDNS(8, 8, 4, 4);    // Резервный ДНС (опционально)
 
 bool ledState1 = 0;
 bool ledState2 = 0;
+bool ledState3 = 0;
 
-// Notify customers about the current status of the LED
-void notifyClients1() {ws.textAll(String(ledState1)); }
-void notifyClients2() {ws.textAll(String(ledState2+2)); }
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
-/* функция обратного вызова, которая запускается всякий раз, когда мы получаем новые
-  данные от клиентов по протоколу WebSocket. Если мы получаем сообщение “toggle”, мы
-  переключаем значение переменной ledState. Кроме того, мы уведомляем всех клиентов,
-  вызывая функцию notifyClients (). Таким образом, все клиенты получают уведомление об
-  изменении и соответствующим образом обновляют интерфейс.*/
+// Notify clients about the current status of the LED
+void notifyClients1() {ws.textAll(String(ledState1));}
+void notifyClients2() {ws.textAll(String(ledState2+2));}
+void notifyClients3() {ws.textAll(String(ledState3+4));}
+  
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
     data[len] = 0;
-    if (strcmp((char*)data, "toggle1") == 0) {
-      ledState1 = !ledState1;
-      notifyClients1();
-    }
-    if (strcmp((char*)data, "toggle2") == 0) {
-      ledState2 = !ledState2;
-      notifyClients2();
-    }
+      if (strcmp((char*)data, "toggle1") == 0) {ledState1 = !ledState1; notifyClients1();} 
+      if (strcmp((char*)data, "toggle2") == 0) {ledState2 = !ledState2; notifyClients2();}
+	  if (strcmp((char*)data, "toggle3") == 0) {ledState3 = !ledState3; notifyClients3();}
   }
 }
 
-
-// Настройка прослушивателя событий для обработки различных асинхронных шагов протокола WebSocket
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
              void *arg, uint8_t *data, size_t len) {
   switch (type) {
-    case WS_EVT_CONNECT:                  // когда клиент вошел в систему
+    case WS_EVT_CONNECT:                 
       Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
       break;
-    case WS_EVT_DISCONNECT:               // когда клиент вышел из системы
+    case WS_EVT_DISCONNECT:              
       Serial.printf("WebSocket client #%u disconnected\n", client->id());
       break;
-    case WS_EVT_DATA:                     // при получении пакета данных от клиента
+    case WS_EVT_DATA:                    
       handleWebSocketMessage(arg, data, len);
       break;
-    case WS_EVT_PONG:                     // в ответ на запрос ping
-    case WS_EVT_ERROR:                    // при получении ошибки от клиента
+    case WS_EVT_PONG:                    
+    case WS_EVT_ERROR:                   
       break;
   }
 }
@@ -70,37 +61,21 @@ void initWebSocket() {
 
 String processor(const String& var) {
   Serial.println(var);
-  if (var == "STATE1") {
-    if (ledState1) {
-      return "ON";
-    }
-    else {
-      return "OFF";
-    }
-  }
-
-  if (var == "STATE2") {
-    if (ledState2) {
-      return "ON";
-    }
-    else {
-      return "OFF";
-    }
-  }
+  if (var == "STATE1") {if (ledState1) {return "ON";}else {return "OFF";}}
+  if (var == "STATE2") {if (ledState2) {return "ON";}else {return "OFF";}}
+  if (var == "STATE3") {if (ledState3) {return "ON";}else {return "OFF";}}
 }
 
 void setup() {
   // Serial port for debugging purposes
   Serial.begin(115200);
 
-  pinMode(32, OUTPUT);
-  digitalWrite(32, LOW);
-  pinMode(33, OUTPUT);
-  digitalWrite(33, LOW);
+  pinMode(32, OUTPUT); digitalWrite(32, LOW);
+  pinMode(33, OUTPUT); digitalWrite(33, LOW);
+  pinMode(25, OUTPUT); digitalWrite(25, LOW);
 
-  // Настраиваем статический IP-адрес:
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-    Serial.println("Режим клиента не удалось настроить");
+    Serial.println("Client mode could not be configured");
   }
 
   if (!SPIFFS.begin()) {
@@ -120,7 +95,7 @@ void setup() {
 
   initWebSocket();
 
-  // Route for root / web page
+ // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
     request->send(SPIFFS, "/index.html", "text/html");
   });
@@ -130,7 +105,7 @@ void setup() {
   });
 
   server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/script.js", "text/javascript");
+   request->send(SPIFFS, "/script.js", "text/javascript");
   });
 
   // Start server
@@ -141,4 +116,5 @@ void loop() {
   ws.cleanupClients();
   digitalWrite(32, ledState1);
   digitalWrite(33, ledState2);
+  digitalWrite(25, ledState3);
 }
